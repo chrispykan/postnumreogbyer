54342c8f2921debfee97890201aef658f1b54353
chrispykan <christineparisk@gmail.com>
2021-02-19 15:18:17 +0100

    initial commit

---
+++ src/main/java/com/chrispykan/postnumreogbyer/PostnumreogbyerApplication.java
@@ -0,0 +1,13 @@
+package com.chrispykan.postnumreogbyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostnumreogbyerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostnumreogbyerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chrispykan/postnumreogbyer/PostnumreogbyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrispykan.postnumreogbyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostnumreogbyerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
